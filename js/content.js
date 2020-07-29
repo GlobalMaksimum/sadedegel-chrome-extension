9f5bdf684ab0f5ada78b69af4384f5d09f923594
Murat Can Çakır <murat.cakir@globalmaksimum.com>
2020-07-29 19:23:49 +0300

    Add dynamic read duration calculation

--- js/content.js
+++ js/content.js
@@ -1,5 +1,6 @@
 // Constants
-const api = "http://localhost:8000/sadedegel/random";
+const api = "https://sadedegel.herokuapp.com/api/summarizer/rouge1";
+const apiStats = "https://sadedegel.herokuapp.com/api/doc/statistics";
 
 // Listen message from background script
 chrome.runtime.onMessage.addListener(messageReceiver)
@@ -18,19 +19,67 @@ function messageReceiver(message, sender, sendResponse) {
     } else {
         // Create modal from scratch
         const selectedArticleText = identifyArticleText(message.url);
-        summarize(selectedArticleText, 1)
-            .then(summaryText => {
-                loadSummaryModal(summaryText);
-            });
+        getArticleDurations(selectedArticleText).then(durations => {
+            console.log(durations.radio2);
+            summarize(selectedArticleText, durations.radio2)
+                .then(summaryText => {
+                    loadSummaryModal(summaryText, durations);
+                });
+        }
+        );
     }
 }
 
-// Summarization api call
-async function summarize(article, articleLength) {
-    console.log(article);
+async function getArticleDurations(article) {
     let jsonBody = {};
-    jsonBody["text"] = article;
-    jsonBody["articleLength"] = articleLength;
+    jsonBody["doc"] = article;
+
+    const response = await fetch(apiStats, {
+        method: 'POST',
+        body: JSON.stringify(jsonBody),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(data => {
+            console.log("total duration" + data.duration);
+            let durations = {
+                radio1: calculateDuration(data.duration, 0.1),
+                radio2: calculateDuration(data.duration, 0.5),
+                radio3: calculateDuration(data.duration, 0.8),
+            }
+            console.log("api call return object");
+            console.log(durations)
+            return durations
+        });
+    return response;
+}
+
+function calculateDuration(seconds, factor) {
+    let s = seconds * factor;
+    let r = (Math.ceil(s / 10) * 10)
+
+    if (r < 60) {
+        return { duration: r, unit: "second" };
+    } else {
+        return { duration: Math.ceil(r / 60), unit: "minute" };
+    }
+}
+
+
+// Summarization api call
+async function summarize(article, durationObj) {
+    let jsonBody = {};
+    jsonBody["doc"] = article;
+    jsonBody["duration"] = durationObj.duration;
+    jsonBody["unit"] = durationObj.unit;
+    console.log("jsonbody");
+    console.log(jsonBody);
 
     const response = await fetch(api, {
         method: 'POST',
@@ -46,7 +95,7 @@ async function summarize(article, articleLength) {
     }).then(res => res.json())
         .then(data => {
             let summaryText = '';
-            data.sentences.map(sentence => summaryText += ' ' + sentence);
+            data.sentences.map(sentence => summaryText += sentence);
             return summaryText;
         });
     return response;
@@ -75,7 +124,7 @@ function identifyArticleText(url) {
 
 
 // Insert modal object to DOM
-function loadSummaryModal(summaryText) {
+function loadSummaryModal(summaryText, durations) {
 
     // Bootstrap modal
     let summaryModal = document.createElement("div");
@@ -149,27 +198,36 @@ function loadSummaryModal(summaryText) {
     let optWrapper3 = document.createElement('div');
     optWrapper3.className = 'form-check';
     let option1label = document.createElement('label');
+    option1label.id = "duration1label"
     option1label.className = 'form-check-label';
-    option1label.textContent = '1 dakikalık';
+    option1label.textContent = durations.radio1.duration + ' ' + (durations.radio1.unit == 'second' ? 'saniye' : 'dakika');
     let option2label = document.createElement('label');
-    option2label.className = 'form-check-label';
-    option2label.textContent = '5 dakikalık';
+    option2label.id = "duration2label"
+    option2label.className = 'form-check-label active';
+    option2label.textContent = durations.radio2.duration + ' ' + (durations.radio2.unit == 'second' ? 'saniye' : 'dakika');
     let option3label = document.createElement('label');
+    option3label.id = "duration3label"
     option3label.className = 'form-check-label';
-    option3label.textContent = '10 dakikalık';
+    option3label.textContent = durations.radio3.duration + ' ' + (durations.radio3.unit == 'second' ? 'saniye' : 'dakika');
     let opt1 = document.createElement('input');
+    opt1.id = "duration1";
     opt1.setAttribute('type', 'radio');
-    opt1.setAttribute('value', '0');
+    opt1.setAttribute('value', durations.radio1.duration);
     opt1.setAttribute('name', 'length');
+    opt1.setAttribute('data-unit', durations.radio1.unit);
     let opt2 = document.createElement('input');
+    opt2.id = "duration2";
     opt2.setAttribute('type', 'radio');
-    opt2.setAttribute('value', '1');
+    opt2.setAttribute('value', durations.radio2.duration);
     opt2.setAttribute('name', 'length');
     opt2.setAttribute('checked', true);
+    opt2.setAttribute('data-unit', durations.radio2.unit);
     let opt3 = document.createElement('input');
+    opt3.id = "duration3";
     opt3.setAttribute('type', 'radio');
-    opt3.setAttribute('value', '2');
+    opt3.setAttribute('value', durations.radio3.duration);
     opt3.setAttribute('name', 'length');
+    opt3.setAttribute('data-unit', durations.radio3.unit);
 
     //placing the options
     option1label.appendChild(opt1);
@@ -198,6 +256,11 @@ function loadSummaryModal(summaryText) {
     const radios = document.querySelectorAll('input[type=radio][name=length]');
     radios.forEach(radio => radio.addEventListener('change', changeSummaryLength));
 
+    $('#optionsWrapper .form-check-label').click((e) => {
+        $('#optionsWrapper').find('.active').removeClass('active');
+        e.target.parentNode.classList.add('active');
+    });
+
     // Show modal
     $('#summaryModal').modal({
         keyboard: false
@@ -205,9 +268,9 @@ function loadSummaryModal(summaryText) {
 }
 
 function changeSummaryLength(event) {
-    const summaryLength = event.target.value;
-    const apiCallPromise = summarize(identifyArticleText(event.target.baseURI), summaryLength);
-
+    const duration = event.target.value;
+    const unit = document.getElementById(event.target.id).getAttribute("data-unit")
+    const apiCallPromise = summarize(identifyArticleText(event.target.baseURI), { duration: duration, unit: unit });
     apiCallPromise.then(summaryText => {
         document.getElementById("summaryTextResponse").innerHTML = summaryText;
     });
